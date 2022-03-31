060d3fdb8f72646d1c44a3aa1b7ed2319187ee63
Illés Tóth <illestothvisualz@gmail.com>
2022-03-31 18:24:06 +0200

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,256 @@
+<template>
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined' && 26 >= weather.main.temp >= 16
+        ? 'normal'
+        : typeof weather.main != 'undefined' && weather.main.temp < 16
+        ? 'cold'
+        : typeof weather.main != 'undefined' && weather.main.temp > 26
+        ? 'warm'
+        : ''
+    "
+  >
+    <main
+      :class="
+        typeof weather.main != 'undefined' &&
+        weather.weather[0].main == 'Clouds'
+          ? 'clouds'
+          : typeof weather.main != 'undefined' &&
+            weather.weather[0].main == 'Snow'
+          ? 'snow'
+          : typeof weather.main != 'undefined' &&
+            weather.weather[0].main == 'Rain'
+          ? 'rain'
+          : typeof weather.main != 'undefined' &&
+            weather.weather[0].main == 'Haze'
+          ? 'haze'
+          : ''
+      "
+    >
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">
+            <div id="app-name" v-show="typeof weather.main === 'undefined'">
+              <h3>This is a weather app.</h3>
+              <h5>Enter a city name in the search field.</h5>
+            </div>
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }} °C</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "995387fcb5bc094ed802de8105ccd9f1",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Righteous&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Righteous", cursive;
+  letter-spacing: 3px;
+}
+
+#app {
+  background-image: url("./assets/normal-bg.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+#app-name {
+  display: none;
+}
+
+#app.cold {
+  background-image: url("./assets/cold-bg.jpg");
+  background-position: bottom;
+}
+
+#app.warm {
+  background-image: url("./assets/warm-bg.jpg");
+  background-position: bottom;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-size: cover;
+  opacity: 0.8;
+}
+
+main.clouds {
+  background-image: url("./assets/clouds.png");
+  opacity: 1;
+}
+
+main.snow {
+  background-image: url("./assets/snow.jpg");
+}
+
+main.rain {
+  background-image: url("./assets/rain.png");
+}
+
+main.haze {
+  background-image: url("./assets/haze.png");
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 3px 6px rgb(0, 0, 0, 0.75);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+  background-color: rgba(0, 0, 0, 0.45);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+}
+
+.none {
+  display: none;
+}
+
+.show {
+  display: block;
+}
+</style>
