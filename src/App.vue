2de0c529f6891351cd24438a4ae9315dc039ecf4
Illés Tóth <illestothvisualz@gmail.com>
2022-03-31 19:39:36 +0200

    class-binding/sass-loader

--- src/App.vue
+++ src/App.vue
@@ -38,13 +38,14 @@
         />
       </div>
 
+      <div id="app-intro">
+        <h1>This is a weather app.</h1>
+        <h3>Enter a city name in the search field.</h3>
+      </div>
+
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">
-            <div id="app-name" v-show="typeof weather.main === 'undefined'">
-              <h3>This is a weather app.</h3>
-              <h5>Enter a city name in the search field.</h5>
-            </div>
             {{ weather.name }}, {{ weather.sys.country }}
           </div>
           <div class="date">{{ dateBuilder() }}</div>
@@ -72,6 +73,7 @@ export default {
   },
   methods: {
     fetchWeather(e) {
+      document.getElementById("app-intro").style.display = "none";
       if (e.key == "Enter") {
         fetch(
           `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
@@ -120,7 +122,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Righteous&display=swap");
 
 * {
@@ -139,20 +141,24 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+  .cold {
+    background-image: url("./assets/cold-bg.jpg");
+    background-position: bottom;
+  }
+  .warm {
+    background-image: url("./assets/warm-bg.jpg");
+    background-position: bottom;
+  }
 }
 
-#app-name {
-  display: none;
-}
-
-#app.cold {
-  background-image: url("./assets/cold-bg.jpg");
-  background-position: bottom;
-}
-
-#app.warm {
-  background-image: url("./assets/warm-bg.jpg");
-  background-position: bottom;
+#app-intro {
+  color: #fff;
+  text-align: center;
+  margin-top: 10vh;
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 0.7em;
+  }
 }
 
 main {
@@ -166,15 +172,12 @@ main.clouds {
   background-image: url("./assets/clouds.png");
   opacity: 1;
 }
-
 main.snow {
-  background-image: url("./assets/snow.jpg");
+  background-image: url("./assets/snow.png");
 }
-
 main.rain {
   background-image: url("./assets/rain.png");
 }
-
 main.haze {
   background-image: url("./assets/haze.png");
 }
@@ -182,68 +185,66 @@ main.haze {
 .search-box {
   width: 100%;
   margin-bottom: 30px;
+  .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline: none;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 0px 16px;
+    transition: 0.4s;
+    &:focus {
+      box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+      background-color: rgba(255, 255, 255, 0.75);
+      border-radius: 16px 0px;
+    }
+  }
 }
 
-.search-box .search-bar {
-  display: block;
-  width: 100%;
-  padding: 15px;
-  color: #313131;
-  font-size: 20px;
-  appearance: none;
-  border: none;
-  outline: none;
-  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255, 255, 255, 0.5);
-  border-radius: 0px 16px;
-  transition: 0.4s;
-}
-
-.search-box .search-bar:focus {
-  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 16px 0px;
-}
-
-.location-box .location {
-  color: #fff;
-  font-size: 32px;
-  font-weight: 500;
-  text-align: center;
-  text-shadow: 3px 6px rgb(0, 0, 0, 0.75);
-}
-
-.location-box .date {
-  color: #fff;
-  font-size: 20px;
-  font-weight: 300;
-  font-style: italic;
-  text-align: center;
+.location-box {
+  .location {
+    color: #fff;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 3px 6px rgb(0, 0, 0, 0.75);
+  }
+  .date {
+    color: #fff;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
 }
 
 .weather-box {
   text-align: center;
-}
+  .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #fff;
+    font-size: 102px;
+    font-weight: 900;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+    background-color: rgba(0, 0, 0, 0.45);
+    border-radius: 16px;
+    margin: 30px 0px;
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+  }
 
-.weather-box .temp {
-  display: inline-block;
-  padding: 10px 25px;
-  color: #fff;
-  font-size: 102px;
-  font-weight: 900;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.75);
-  background-color: rgba(0, 0, 0, 0.45);
-  border-radius: 16px;
-  margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.75);
-}
-
-.weather-box .weather {
-  color: #fff;
-  font-size: 48px;
-  font-weight: 700;
-  font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+  .weather {
+    color: #fff;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+  }
 }
 
 .none {
